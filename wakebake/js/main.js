df82f7ced370a151fd09aee4e175fefa040754f5
Aleksey  │ Frontend Dev <aleksejkorotihin1002@gmail.com>
2025-08-15 15:22:19 +0300

    Add files via upload

--- wakebake/js/main.js
+++ wakebake/js/main.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
+    document.addEventListener('click', burgerInit);
 
-    document.addEventListener('click', burgerInit  )
+    function burgerInit(e) {
+        const burgerIcon = e.target.closest('.burger__icon');
+        const burgerNavLink = e.target.closest('.nav__link');
 
-    function burgerInit(e){
+        if (!burgerIcon && !burgerNavLink) return;
 
-        const burgerIcon = e.target.closest('.burger__icon')
-        const burgerNavLink = e.target.closest('nav__link')
+        if (document.documentElement.clientWidth > 900) return;
+        if (burgerIcon) {
+            document.body.classList.toggle('body--opened-menu');
+        }
 
-        if (!burgerIcon && burgerNavLink) return
-        if (document.documentElement.clientWidth > 900) return
-
-        if(!document.body.classList.contains('body--opened-menu')){
-            document.body.classList.add('body--opened-menu')
-        } else {
-            document.body.classList.remove('body--opened-menu')
+        if (burgerNavLink) {
+            document.body.classList.remove('body--opened-menu');
         }
     }
-})()
+})();
